e0ea59d0f333c9d4d2b6694842b289521aec4d74
kedehboy <ayangcostly@gmail.com>
2023-07-04 16:42:09 +0100

    Updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,9 @@
+'use client'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 import { Toaster } from 'react-hot-toast'
 
-export const metadata = {
-  title: 'Opulenzofficial',
-  description: 'Official Website for Opulenzofficial',
-}
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
